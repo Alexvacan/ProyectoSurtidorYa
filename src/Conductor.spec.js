628c9fa1141d4a63af47312edaa3d18e5783b819
chenaoh <alexander.vacaflores@ucb.edu.bo>
2025-04-24 08:27:53 -0400

    Correcciones

--- src/Conductor.spec.js
+++ src/Conductor.spec.js
@@ -1,5 +1,10 @@
 import { Conductor } from './Conductor';
 
+global.localStorage = {
+    getItem: jest.fn(() => null), // simulamos que no hay datos guardados
+    setItem: jest.fn(),
+  };
+
 describe('Conductor', () => {
   it('debería retornar una lista de surtidores', () => {
     const conductor = new Conductor();
@@ -11,4 +16,16 @@ describe('Conductor', () => {
     expect(surtidores[0]).toHaveProperty('estado');
     expect(surtidores[0]).toHaveProperty('fila');
   });
+
+  it('debería guardar en localStorage al agregar un surtidor', () => {
+    const conductor = new Conductor();
+    conductor.agregarSurtidor('Nuevo', 'Disponible', 3);
+    
+    expect(localStorage.setItem).toHaveBeenCalled();
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'surtidores',
+      expect.stringContaining('Nuevo')
+    );
+  });
+  
 });

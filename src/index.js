c8dfca5eac924723b040ecdd6e5914a1a4cd74ac
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2025-05-04 10:59:06 -0400

    fix: correccion del index.js para una correcta ejecucion

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-// src/index.js
-console.log('El proyecto se ha cargado correctamente.');
-
 import { Presenter } from './presenter.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('Hola, el proyecto se ha cargado correctamente');
   const presenter = new Presenter();
   presenter.inicializar();
 });
-
-// Nota:
-// — Hemos eliminado la clase GestorReabastecimiento y su listener de form-surtidor
-//   porque Presenter ya se encarga de capturar #form-surtidor en manejarFormulario()
-//   (y de agregar y renderizar los surtidores, incluyendo ordenación).
-// — De este modo, #form-surtidor funciona correctamente junto con el sort-criteria.
